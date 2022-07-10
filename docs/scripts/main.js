283fbff8458d1ec8d0d1dbb01a1283f823eebac1
Squirrel Modeller <87671560+SquirrelModeller@users.noreply.github.com>
2022-07-10 09:08:26 +0000

    Update main.js

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -2,7 +2,7 @@ if ('serviceWorker' in navigator) {
     console.log('Service Worker Supported');
     window.addEventListener('load', () => {
         navigator.serviceWorker
-            .register('../sw_cached_pages.js')
+            .register('/Haruhi-Suzumiya-3D-School/scripts/main.js/sw_cached_pages.js')
             .then(reg => console.log('Service Worker: Registered'))
             .catch(err => console.log(`Service Worker: Error: ${err}`));
     })
